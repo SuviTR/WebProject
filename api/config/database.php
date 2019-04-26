71a0852d02e5d52792fa155233e4096f6960956d
Suvi Rannisto <suvi.rannisto@metropolia.fi>
2019-04-26 15:05:28 +0300

    Api added

---
+++ api/config/database.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Author: suviTR & heikkeket
+ * Date: 26.4.2019
+ * Time: 11.55
+ */
+class Database{
+
+    //Specify your own database credentials
+    private $host = "localhost";
+    private $db_name = "cv_db";
+    private $username = "olso";
+    private $password = "olso";
+    public $conn;
+
+    //The database connection
+    public function getConnection(){
+
+        $this->conn = null;
+
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+
+?>
