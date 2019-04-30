46b3beb0954773219c95ec9534c0b847fb714083
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2019-04-30 11:09:06 +0300

    Added INSERT clause to import some data to the DB

--- api/db.sql
+++ api/db.sql
@@ -1,3 +1,11 @@
+DROP TABLE IF EXISTS 'CV';
+DROP TABLE IF EXISTS 'Skills';
+DROP TABLE IF EXISTS 'Experience';
+DROP TABLE IF EXISTS 'Education';
+DROP TABLE IF EXISTS 'Some';
+DROP TABLE IF EXISTS 'Project';
+DROP TABLE IF EXISTS 'Pictures';
+
 CREATE TABLE CV
 (
   Fullname INT NOT NULL,
@@ -77,3 +85,19 @@ CREATE TABLE Pictures
   PRIMARY KEY (Id),
   FOREIGN KEY (PId) REFERENCES Project(PId)
 );
+
+INSERT INTO CV (Id, Fullname, FrontPicture, AboutPicture, Profession, Heading,
+Description, Call, Mail, Address)
+VALUES (1, "Jane Doe", "img/cv_janeDoe_cropped2_darkened.jpg",
+"img/cv_janeDoe2_cropped.jpg", "Software engineer", "Hello! I'm Jane",
+"I am energetic software engineer
+                    with 4 years experience developing robust code for high-volume businesses.
+                    I'm a hard working, flexible and reliable person,
+                    who learns quickly and willing to undertake any task given me.
+                    I enjoy meeting people and work well as part of a team or on my own.
+                    I am also fun and caring. My family including my sweet dog means everything to me.
+                    I love hanging out with my family and friends.
+                    On my spare time I enjoy reading, going hiking and just walking in nature.
+                    <br><br><button class="resumebutton" onclick="window.location.href = 'resume.html';">See My Resume</button>
+                </p>",
+"123-456-7890​", "jane.doe(at)mail.com", "Example Street 10<br>London, UK");
