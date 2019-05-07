eec35ec8aad783568193eae123f42e1741371a19
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2019-05-07 10:16:59 +0300

    Most of the frontpage works

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
-window.addEventListener('load', () => {
-	const el = $('#app');
-	const serv = 'http://vcust539.louhi.net:81';
-	var json;
+const el = $('#app');
+const serv = 'http://vcust539.louhi.net:81';
 
+
+window.addEventListener('load', app);
+
+function app() {
 
 	const router = new Router({
 		mode: 'history',
@@ -12,19 +14,19 @@ window.addEventListener('load', () => {
 	router.add('/', () => {
 		showTemplate('index');
 
-		getAjaxData('/cv/front', function(json) {
-			document.getElementById('fullname').innerHTML=json.fullname;
-			document.getElementById('profession')
-			.innerHTML=json.profession;
-			document.getElementById('frontpicture').setAttribute('src', json.FrontPicture);
-		});
+		getAjaxData('/cv/front', showFront);
 
-		getAjaxData('/cv/about', function(json) {
-			document.getElementById('aboutheading').innerHTML=json.heading;
-			document.getElementById('aboutdescription')
-			.innerHTML=json.description;
-			document.getElementById('aboutimg').setAttribute('src', json.picture);
-		});
+		getAjaxData('/cv/about', showAbout);
+
+		getAjaxData('/cv/skills', showSkills);
+
+		getAjaxData('/cv/experience', showExperience);
+
+		getAjaxData('/cv/education', showEducation);
+
+		getAjaxData('/cv/portfolio', showPortfolio);
+
+		getAjaxData('/cv/concact', showContact);
 	})
 
 	router.add('/portfolio/project1', () => {
@@ -36,44 +38,136 @@ window.addEventListener('load', () => {
 	const link = $(`a[href$='${window.location.pathname}']`);
 	link.addClass('active');
 
+}
 
-	/* Contents functions */
-	function getAjaxData(address, processingFunction) {
-		httpRequest = new XMLHttpRequest();
-		httpRequest.onreadystatechange = function () {
-			if(httpRequest.readyState === XMLHttpRequest.DONE) {
-				if(httpRequest.status === 200 ) {
-					var json = JSON.parse(httpRequest.responseText)
-					processingFunction(json);
-				}
+/* Contents functions */
+function getAjaxData(address, processingFunction) {
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.onreadystatechange = function () {
+		if(httpRequest.readyState === XMLHttpRequest.DONE) {
+			if(httpRequest.status === 200 ) {
+				
+				var json = JSON.parse(httpRequest.responseText)
+				processingFunction(json);
 			}
 		}
-
-		console.log('address: ' + serv + address);
-		httpRequest.open('GET', serv + address, true);
-		httpRequest.send();
 	}
 
+	console.log('address: ' + serv + address);
+	httpRequest.open('GET', serv + address, true);
+	httpRequest.send();
+}
 
-	function showTemplate(templateName) {
-		let template = document.getElementById(templateName);
-		let app = document.getElementById('app');
-		app.innerHTML = "";
-		app.appendChild(template.content.cloneNode(true));
 
-		$('a.spa').on('click', (event) => {
-			event.preventDefault();
+function showTemplate(templateName) {
+	let template = document.getElementById(templateName);
+	let app = document.getElementById('app');
+	app.textContent = "";
+	app.appendChild(template.content.cloneNode(true));
 
-			var target = $(event.target).closest('a');
+	$('a.spa').on('click', (event) => {
+		event.preventDefault();
 
-			const href = target.attr('href');
-			const path = href;
+		var target = $(event.target).closest('a');
 
-			console.log(path);
-			router.navigateTo(path);
-		});
+		const href = target.attr('href');
+		const path = href;
+
+		console.log(path);
+		router.navigateTo(path);
+	});
+}
+
+
+function showFront(json) {
+			json = json[0];
+			document.getElementById('fullname').textContent=json.Fullname;
+			document.getElementById('profession')
+			.textContent=json.Profession;
+			document.getElementById('frontpicture').setAttribute('src', json.FrontPicture);
+}
+
+function showAbout(json) {
+			json = json[0];
+			document.getElementById('aboutheading').textContent=json.Heading;
+			document.getElementById('aboutdescription')
+			.innerHTML=json.Description;
+			document.getElementById('aboutimg').setAttribute('src', json.AboutPicture);
+		}
+
+function showSkills(json) {
+	var id = document.getElementById('skillsdiv');
+	var table = id.getElementsByTagName('table')[0];
+	var template = document.getElementById('skillsrow');
+
+	for(var i = 0; i < json.length; i++) {
+		var row = document.importNode(template.content, true);
+		row.querySelector('.skillsname').textContent
+		= json[i].Name;
+		
+		var bar = row.querySelector('.skillsbarvalue');
+		bar.setAttribute('class', 'skillsbarvalue value-' + json[i].SkillLevel);
+		row.querySelector('.skillsvalue').textContent
+		= json[i].SkillLevel;
+		table.appendChild(row);
+
 	}
+}
 
+function showExperience(json) {
+	var id = document.getElementById('expdiv');
+	var table = id.getElementsByTagName('table')[0];
+	var template = document.getElementById('exprow');
 
+	for(var i = 0; i < json.length; i++) {
+		var row = document.importNode(template.content, true);
+		row.querySelector('.time').textContent
+		= json[i].Exp_Year;
+		row.querySelector('.experiencep').textContent
+		= json[i].Title;
+		row.querySelector('.expplace').textContent
+		= json[i].Company;
+		row.querySelector('.expdesc').textContent
+		= json[i].Description;
+		table.appendChild(row);
 
-})
+	}
+}
+
+function showEducation(json) {
+	var id = document.getElementById('edudiv');
+	var table = id.getElementsByTagName('table')[0];
+	var template = document.getElementById('edurow');
+
+	console.log(template);
+
+	for(var i = 0; i < json.length; i++) {
+		var row = document.importNode(template.content, true);
+
+		row.querySelector('.time').textContent
+		= json[i].Edu_Year;
+		row.querySelector('.experiencep').textContent
+		= json[i].Degree;
+		row.querySelector('.educationp').textContent
+		= json[i].Academy;
+		row.querySelector('.educationpi').textContent
+		= json[i].Description;
+		table.appendChild(row);
+
+	}
+}
+
+function showPortfolio(json) {
+
+}
+
+function showContact(json) {
+	json = json[0];
+	//     $sql = "SELECT Phone, Mail, Address, Name, Link ,SomeIcon FROM 
+	var div = document.getElementById('contact');
+
+	contact.getElementById('phone_number').textContent=json.Phone;
+	contact.getElementById('email_address').textContent=json.Mail;
+	contact.getElementById('street_address').textContent=json.Address;
+
+}
