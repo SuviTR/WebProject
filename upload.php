30e27a02f2dce8098bac2e9ae95d8f7d31225bdc
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2019-05-09 15:34:20 +0300

    upload.php prints JSON

--- upload.php
+++ upload.php
@@ -10,44 +10,48 @@ function uploadImage(){
     $target_file = $target_dir . basename($_FILES["upload"]["name"]);
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-    echo $target_file;
+    $rows['file'] = $target_file;
     // Check if image file is a actual image or fake image
     if(isset($_POST["submit"])) {
         $check = getimagesize($_FILES["upload"]["tmp_name"]);
         if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
+            $rows['Message'] =  "File is an image - " . $check["mime"] . ".";
             $uploadOk = 1;
         } else {
-            echo "File is not an image.";
+            $rows['Message'] =  "File is not an image.";
             $uploadOk = 0;
         }
     }
     // Check if file already exists
     if (file_exists($target_file)) {
-        echo "File already exists.";
+        $rows['Message'] =  "File already exists.";
         $uploadOk = 0;
     }
     // Check file size
     if ($_FILES["upload"]["size"] > 500000) {
-        echo "File is too large.";
+        $rows['Message'] =  "File is too large.";
         $uploadOk = 0;
     }
     // Allow certain file formats
     if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
         && $imageFileType != "gif" ) {
-        echo "Only JPG, JPEG, PNG & GIF files are allowed.";
+        $rows['Message'] =  "Only JPG, JPEG, PNG & GIF files are allowed.";
         $uploadOk = 0;
     }
     // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
-        echo "File was not uploaded.";
+        $rows['Message'] =  "File was not uploaded.";
     // if everything is ok, try to upload file
     } else {
         if (move_uploaded_file($_FILES["upload"]["tmp_name"], $target_file)) {
-            echo "The file ". basename( $_FILES["upload"]["name"]). " has been uploaded.";
+            $rows['Message'] =  "The file ". basename( $_FILES["upload"]["name"]). " has been uploaded.";
         } else {
-            echo "There was an error uploading your file.";
+            $rows['Message'] =  "There was an error uploading your file.";
         }
     }
+
+    header("Content-Type: application/json; charset=UTF-8");
+    echo json_encode($rows);
+
 }
 ?>
